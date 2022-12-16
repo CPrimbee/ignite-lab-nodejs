ddbe743322e031137c7784041cb676245372299e
Cristiano Gonçalves <cprimbee@gmail.com>
2022-12-16 13:58:47 -0300

    2nd class

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  roots: ['src', 'test'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+};
